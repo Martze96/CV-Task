df7280fdfce14e66e914e8f9a710860ef9d94d0b
Martin Maul <martin.maul@hs-weingarten.de>
2021-10-24 19:17:46 +0200

    design fun

--- src/App.js
+++ src/App.js
@@ -120,7 +120,6 @@ class App extends Component {
     
     return (
     <div className="App">
-      <div className="background"></div>
       <div className="header">CV-Maker with React.js</div>
       <GeneralSection
         name={name}
